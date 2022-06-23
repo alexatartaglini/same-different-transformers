5fdeb139f5acb0c0d60f9936740742d7d27eaf65
Alexa Tartaglini <genghis0swan@gmail.com>
2022-06-23 17:03:02 -0600

    Cleaned up code

--- data.py
+++ data.py
@@ -6,12 +6,7 @@ import argparse
 import glob
 from torch.utils.data import Dataset
 import itertools
-from math import factorial, floor
-
-
-def nCr(n, r):
-    f = factorial
-    return f(n) / f(r) / f(n-r)
+from math import floor
 
 
 def load_dataset(root_dir):
@@ -57,108 +52,33 @@ class SameDifferentDataset(Dataset):
         return item, im_path
 
 
-
-def initialize_pairs(k, n, object_files, obj_size, im_size):
-    '''
-    Creates len(object_files) unique k-length combinations selected from object_files without
-    the computational cost incurred by itertools, which generates all possible
-    combinations and becomes intractable when k = 8 and n > 100. Then, all permutations
-    where the same object is repeated k times are appended (same condition).
-
-    :param k: length of combination.
-    :param n: number of different pairs to produce (total # pairs = n * 2). If n == 'MAX', n
-              is set to the total number of possible different object pairs.
-    :param object_files: population to sample from.
-
-    :returns: a length n list of different object pairs, and a length n list of same object pairs.
-              Note that if n > len(object_files), the same list will contain repeats, and if
-              n > len(all possible different-object combinations), then the different list will
-              contain repeats.
-    '''
-
-    all_combos = list(itertools.combinations(object_files, k))
-    lim = (im_size // obj_size)**2
-    lim = nCr(lim, k)
-
-    obj_counts = {o: 0 for o in object_files}
-
-    if n == 'MAX':
-        n = len(all_combos)
-
-    if n <= len(all_combos):
-        different_pairs = random.sample(all_combos, n)
-    else:
-        different_pairs = [random.sample(object_files, k) for _ in range(n)]
-
-    same_pairs = []
-    for _ in range(n):
-        obj = random.choice(object_files)
-
-        while obj_counts[obj] == lim:
-            obj = random.choice(object_files)
-
-        same_pairs.append([obj for _ in range(k)])
-        obj_counts[obj] += 1
-
-    return different_pairs, same_pairs
-
-
-'''
-def create_stimuli(pairs, coords, setting, fixed, unaligned, im_size, patch_size, multiplier):
-    p = 0
-    # This is used to ensure that if the same pair of objects is being used in multiple stimuli,
-    # the objects will be placed in a different configuration than before.
-    placement_dict = {''.join(sorted(list(pair))): [] for pair in pairs}
-
-    obj_size = patch_size * multiplier
-
-    for pair in pairs:
-        key = ''.join(sorted(list(pair)))
-        base = Image.new('RGB', (im_size, im_size), (255, 255, 255))
-
-        while True:  # Choose unique position for a given pair
-            object_coords = []
-
-            for i in range(k):
-                if fixed:  # Place objects in fixed locations
-                    c = coords[i]
-                elif unaligned:  # Randomly position objects anywhere
-                    c = random.sample(list(coords), k=2)
-
-                    if len(object_coords) == 1:
-                        o = object_coords[0]
-
-                        # need to alter the following code for k > 2
-                        while (c[0] >= (o[0] - obj_size) and c[0] <= (o[0] + obj_size)) \
-                                and (c[1] >= (o[1] - obj_size) and c[1] <= (o[1] + obj_size)):
-                            c = random.sample(list(coords), k=2)
-
-                else:  # Randomly position objects in aligned positions
-                    c = random.sample(list(coords), k=2)
-
-                    # Do not repeat object positions
-                    while c in object_coords:
-                        c = random.sample(list(coords), k=2)
-
-                object_coords.append(c)
-
-            if fixed:
-                break
-            else:
-                obj_set = set(tuple(c) for c in object_coords)
-                if not obj_set in placement_dict[key]:
-                    break
-
-        for c in range(len(object_coords)):
-            base.paste(object_ims[pair[c]], box=object_coords[c])
-
-        base.save('{0}/{1}_{2}.png'.format(setting, p, k))
-        p += 1
-        if not fixed:
-            placement_dict[key].append(obj_set)
-'''
 def create_stimuli(k, n, objects, unaligned, patch_size, multiplier, im_size, stim_dir,
                    patch_dir, condition):
+    '''
+    Creates n same_different stimuli with (n // 2) stimuli assigned to each class. If
+    n > the number of unique objects used to create the dataset, then randomly selected
+    object pairs will be repeated in unique randomly selected positions until n unique
+    stimuli are created. This code ensures that the number of repeated stimuli is the
+    same between the 'same' and 'different' classes; for example, if a given object
+    pair in the 'same' set is repeated in unique positions three times, another
+    randomly selected object pair in the 'different' set is repeated in three (separate)
+    unique positions.
+
+    :param k: The number of objects per image (eg. 2).
+    :param n: The total desired size of the stimulus set.
+    :param objects: a list of filenames for each unique object to be used in creating
+                    the set. NOTE: it's possible that not all objects in this list will
+                    be used. The actual objects used are randomly selected.
+    :param unaligned: True if stimuli should be randomly placed rather than aligned with
+                      ViT patches.
+    :param patch_size: Size of ViT patches.
+    :param multiplier: Scalar by which to multiply object size. (object size = patch_size
+                       * multiplier)
+    :param im_size: Size of the base image.
+    :param stim_dir: Directory of the objects to be used in creating the set.
+    :param patch_dir: Relevant location to store the created stimuli.
+    :param condition: train, test, or val.
+    '''
 
     obj_size = patch_size * multiplier
 
@@ -384,22 +304,6 @@ if __name__ == "__main__":
     multiplier = args.multiplier
     stim_dir = args.stim_dir
 
-    '''
-    # Brady Lab:
-    n_train = 1920  # Max number of unique objects to present during training
-    n_val = 240  # Max number of unique objects to present during validation
-    n_test = 240  # Max number of unique objects to present during testing
-    n = 3200  # Number of different stimuli to create. Total # stimuli = n*2.
-    n_2 = 320
-
-    # Developmental:
-    n_train = 204
-    n_val = 26
-    n_test = 26
-    n = 512
-    n_2 = 128
-    '''
-
     # Other parameters
     im_size = 224  # Size of base image
 
@@ -475,12 +379,6 @@ if __name__ == "__main__":
     n_unique_val = floor(n_unique * percent_val)
     n_unique_test = floor(n_unique * percent_test)
 
-    '''
-    object_files_train = random.sample(object_files, k=n_train)
-    object_files_val = [x for x in object_files if x not in object_files_train]
-    object_files_test = random.sample(object_files_val, k=n_test)
-    '''
-
     # Allocate unique objects
     ofs = object_files  # Copy of object_files to sample from
 
@@ -491,18 +389,6 @@ if __name__ == "__main__":
     ofs = [o for o in ofs if o not in object_files_val]
 
     object_files_test = random.sample(ofs, k=n_unique_test)
-
-    '''
-    for x in object_files_test:
-        object_files_val.remove(x)
-
-    object_files_val = random.sample(object_files_val, k=n_val)
-
-
-    assert len(object_files_train) == n_train
-    assert len(object_files_test) == n_test
-    assert len(object_files_val) == n_val
-    '''
 
     assert len(object_files_train) == n_unique_train
     assert len(object_files_val) == n_unique_val
@@ -517,79 +403,3 @@ if __name__ == "__main__":
                    im_size, stim_dir, patch_dir, 'val')
     create_stimuli(k, n_test, object_files_test, unaligned, patch_size, multiplier,
                    im_size, stim_dir, patch_dir, 'test')
-
-    '''
-    object_ims = {}
-
-    for o in object_files:
-        im = Image.open('{0}/{1}'.format(stim_dir, o))
-        im = im.resize((patch_size * multiplier, patch_size * multiplier))
-        object_ims[o] = im
-
-    '''
-    '''
-    train_different_pairs, train_same_pairs = initialize_pairs(k, n, object_files_train,
-                                                               (patch_size * multiplier),
-                                                               im_size)
-    test_different_pairs, test_same_pairs = initialize_pairs(k, n_2, object_files_test,
-                                                             (patch_size * multiplier),
-                                                             im_size)
-    val_different_pairs, val_same_pairs = initialize_pairs(k, n_2, object_files_val,
-                                                           (patch_size * multiplier),
-                                                           im_size)
-
-    different_list = [train_different_pairs, test_different_pairs, val_different_pairs]
-    same_list = [train_same_pairs, test_same_pairs, val_same_pairs]
-
-    coords = np.linspace(0, im_size, num=(im_size // patch_size), endpoint=False, dtype=int)
-    new_coords = []
-    for i in range(0, len(coords) - multiplier + 1, multiplier):
-        new_coords.append(coords[i])
-
-    coords = new_coords
-
-    if unaligned:  # Place randomly
-        coords = np.linspace(0, im_size - (patch_size * multiplier),
-                             num=(im_size - (patch_size * multiplier)), dtype=int)
-    elif fixed:  # Place in fixed coordinates
-        if patch_size == 16:
-            if k == 2:
-                coords = [[80, 96], [128, 96]]
-            elif k == 4:
-                coords = [[80, 80], [80, 128], [128, 80], [128, 128]]
-            elif k == 8:
-                coords = [[80, 80], [32, 80], [176, 80], [128, 80],
-                          [80, 128], [32, 128], [176, 128], [128, 128]]
-        elif patch_size == 32:
-            if k == 2:
-                coords = [[64, 96], [128, 96]]
-            elif k == 4:
-                coords = [[64, 64], [128, 64], [64, 128], [128, 128]]
-            elif k == 8:
-                coords = [[32, 32], [96, 32], [160, 32], [160, 96],
-                          [160, 160], [96, 160], [32, 160], [32, 96]]
-
-    for condition in ['train', 'test', 'val']:
-        try:
-            os.mkdir('{0}/{1}'.format(patch_dir, condition))
-        except FileExistsError:
-            pass
-
-        try:
-            os.mkdir('{0}/{1}/same'.format(patch_dir, condition))
-        except FileExistsError:
-            pass
-
-        try:
-            os.mkdir('{0}/{1}/different'.format(patch_dir, condition))
-        except FileExistsError:
-            pass
-
-    for pairs, condition in zip(different_list, ['train', 'test', 'val']):
-        create_stimuli(pairs, coords, '{0}/{1}/different'.format(patch_dir, condition),
-                       fixed, unaligned, im_size, patch_size, multiplier)
-
-    for pairs, condition in zip(same_list, ['train', 'test', 'val']):
-        create_stimuli(pairs, coords, '{0}/{1}/same'.format(patch_dir, condition),
-                       fixed, unaligned, im_size, patch_size, multiplier)
-    '''
